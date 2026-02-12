457156e6e4220b33c38b4fc2ce2f02e8ddf37809
atharv7219 <atharvmehta7219@gmail.com>
2026-02-12 20:45:27 +0530

    Update problem statement for symmetric matrix check

--- day 12.c
+++ day 12.c
@@ -1,4 +1,4 @@
-*/Problem: Write a program to check whether a given matrix is symmetric. A matrix is said to be symmetric if it is a square matrix and is equal to its transpose (i.e., element at position [i][j] is equal to element at position [j][i] for all valid i and j).
+/*Problem: Write a program to check whether a given matrix is symmetric. A matrix is said to be symmetric if it is a square matrix and is equal to its transpose (i.e., element at position [i][j] is equal to element at position [j][i] for all valid i and j).
 
 Input:
 - First line: two integers m and n representing the number of rows and columns
